df9b4aba75f874b068312ddc117ec6c4c27c53fd
Xavier Piernas Andrés <49491012+xavierpiernas@users.noreply.github.com>
2020-02-13 16:59:19 +0100

    Update account_invoice.py

--- invoices_sale_orders_validation/models/account_invoice.py
+++ invoices_sale_orders_validation/models/account_invoice.py
@@ -9,14 +9,17 @@ from odoo.exceptions import UserError
 class AccountInvoice(models.Model):
     _inherit = 'account.invoice'
 
+    @api.multi
     @api.constrains('amount_untaxed', 'payment_mode_id')
     def amount_validation(self):
-        if self.type == 'out_invoice' and\
-            self.company_id.validation_bool and\
-                self.amount_untaxed >= self.company_id.validation_amount and (
-                    self.payment_mode_id.id in self.company_id.validation_payment_mode.ids or not\
-                        self.company_id.validation_payment_mode
-                            ):
-                                raise UserError(
-                                    _("The invoice exceeds the allowed amount for that payment mode!")
-                                )
+        for sel in self:
+            if sel.type == 'out_invoice' and\
+                sel.company_id.validation_bool and\
+                    sel.amount_untaxed >= sel.company_id.validation_amount and\
+                    (
+                        sel.payment_mode_id.id in sel.company_id.validation_payment_mode.ids or not
+                        sel.company_id.validation_payment_mode
+                    ):
+                raise UserError(
+                    _("The invoice exceeds the allowed amount for that payment mode!")
+                )
